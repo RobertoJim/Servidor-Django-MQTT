a438a259eec1956d16e4cf3ee53917fb12fc988e
RobertoJim <jimenezmejiasroberto@gmail.com>
2021-11-28 12:10:18 +0100

    Implementacion precipitaciones OpenWeatherMap

--- ProyectoMQTT/ProyectoMQTT/weather.py
+++ ProyectoMQTT/ProyectoMQTT/weather.py
@@ -1,6 +1,11 @@
 import json
 import requests
 from datetime import datetime
+from time import sleep
+
+import threading
+
+from ProyectoMQTT.mqtt import *
 
 #Quiero comprobar si dentro de una hora va a llover, si va a llover recojo toldo
 #Tendria que ejecutar esa funcion cada hora(funcion recogerdatos)
@@ -8,29 +13,47 @@ from datetime import datetime
 
 #Ademas, tendria que ejecutar la funcion recogerdatos y el sensor detecta agua para comprobar el viento
 
+api_key = "6159431fde89157c2c7bb8ff8a7e841a"
+lat = "36.720969"
+lon = "-4.474427"
+
+
+def openWeatherMap():
+
+    thread = threading.Thread(target=recogerDatos, daemon=True) #Hilo demonio para que finalice al pulsar Ctrl-C??
+    thread.start()
+
 
 def recogerDatos():
 
-    api_key = "6159431fde89157c2c7bb8ff8a7e841a"
-    lat = "36.720969"
-    lon = "-4.474427"
+    while 1:
 
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
-    response = requests.get(url)
-    data = json.loads(response.text)
-
-    #current = data["current"]["pressure"]
-
-    dt = data["minutely"][60]["dt"]
-    hora = str(datetime.fromtimestamp(dt))[11:] #Convierto la fecha a formato conocido y elimino 11 primeros caracteres (elimino fecha) 
-                                                #para obtener solo la hora
-                                    
-    #prevision = data["hourly"][1]["weather"][0]['description'] #Ejemplo, cielo despejado
-
-    prevision = data["minutely"][60]["precipitation"]
+        url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
+        response = requests.get(url)
+        data = json.loads(response.text)
 
 
-    print("La prevision para las " + hora + " es " + str(prevision))
+        dt = data["minutely"][60]["dt"]
+        hora = str(datetime.fromtimestamp(dt))[11:] #Convierto la fecha a formato conocido y elimino 11 primeros caracteres (elimino fecha) 
+                                                    #para obtener solo la hora
+                                        
+        #prevision = data["hourly"][1]["weather"][0]['description'] #Ejemplo, cielo despejado
 
+        prevision = data["minutely"][60]["precipitation"]
 
-    #current = data["hourly"][0]["pressure"]
+        print("La prevision para las " + hora + " es " + str(prevision))
+
+        
+
+        if prevision > 0:
+
+            client.publish("esp32/toldo","down")
+
+        #if viento > umbral:
+            #recoger toldo
+
+        
+
+        #current = data["hourly"][0]["pressure"]
+
+        sleep(20)
